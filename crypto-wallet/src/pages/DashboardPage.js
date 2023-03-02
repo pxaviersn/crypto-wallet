5e77b345600ba2f67132d24eab9756dc397e9afb
pxaviersn <pauloxaviersn@live.com>
2023-03-02 13:05:14 -0800

    Form and other features added

--- crypto-wallet/src/pages/DashboardPage.js
+++ crypto-wallet/src/pages/DashboardPage.js
@@ -1,14 +1,15 @@
 import Navbar from "../components/Navbar";
+import AddOperation from "../components/AddOperation";
 
 const DashboardPage = () => {
 
     return ( 
       <div className="dashboard">
       <Navbar />
+      <AddOperation />
       </div>
      );
   }
    
   export default DashboardPage;
 
-  
